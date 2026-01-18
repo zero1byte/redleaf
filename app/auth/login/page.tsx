ba72dde710562435b8a419fe3121c28660192ee4
zero1byte <rameshmalisundesha720@gmail.com>
2026-01-18 14:47:48 +0530

    init

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,11 +1,12 @@
 import { LoginForm } from "@/components/login-form";
+import AuthLayout from "../layout";
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <AuthLayout>
       <div className="w-full max-w-sm">
         <LoginForm />
       </div>
-    </div>
+    </AuthLayout>
   );
 }
