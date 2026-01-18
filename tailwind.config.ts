3c66b19ca2d20640d2f6e40690765ae4186d0c4a
zero1byte <rameshmalisundesha720@gmail.com>
2026-01-18 22:39:41 +0530

    bug fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -50,11 +50,6 @@ export default {
   				'3': 'hsl(var(--chart-3))',
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))',
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
   			}
   		},
   		borderRadius: {
